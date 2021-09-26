8a1494162458c19d9d7226542cd94b0ac191c80c
Caleb Denio <cjdenio44@gmail.com>
2021-09-26 15:03:46 -0400

    refactor(models): Remove src/models

--- src/models/mod.rs
+++
@@ -1,7 +0,0 @@
-pub mod app;
-pub mod domain;
-pub mod team;
-pub mod team_user;
-pub mod token;
-pub mod user;
-pub mod whitelist;
--- src/models/whitelist.rs
+++
@@ -1,8 +0,0 @@
-use crate::schema::whitelist;
-
-#[derive(Queryable, Insertable, Identifiable)]
-#[table_name = "whitelist"]
-#[primary_key("slack_user_id")]
-pub struct WhitelistEntry {
-    pub slack_user_id: String,
-}
