9160fc3c2be586b555295c53c4a92b8588ff96c4
anirudhb <anirudhb@users.noreply.github.com>
2021-12-30 19:50:12 -0800

    nix: update flake

--- flake.nix
+++ flake.nix
@@ -14,6 +14,32 @@
     let
       pkgs = nixpkgs.legacyPackages.${system};
       fenixPkgs = fenix.packages.${system};
+      haasApiPackage = { rustPlatform, system, systemd ? false, ... }: with pkgs; let
+        isDarwin = lib.hasSuffix "darwin" system;
+        isLinux = lib.hasSuffix "linux" system;
+      in rustPlatform.buildRustPackage rec {
+        pname = "haas-api";
+        version = "0.0.0";
+        src = ./.;
+        cargoLock = {
+          lockFile = ./Cargo.lock;
+          outputHashes = {
+            "caddy-0.1.0" = "sha256-kiTfY6bz9+rat6UkP+7u7jbp7AQVULl7jWMok12S5D4=";
+          };
+        };
+        buildInputs = [
+          (if systemd then postgresql else postgresql.override { systemd = false; })
+        ] ++ lib.optional isDarwin [
+          darwin.apple_sdk.frameworks.Security
+        ] ++ lib.optional isLinux [
+          openssl
+        ];
+        nativeBuildInputs = with pkgs; lib.optional isLinux [
+          pkg-config
+        ];
+        enableParallelBuilding = true;
+        doCheck = false; # FIXME
+      };
     in rec {
       devShell = pkgs.mkShell {
         nativeBuildInputs = with pkgs; [
@@ -36,13 +62,17 @@
         ];
       };
 
-      packages.haas-api = pkgs.rustPlatform.buildRustPackage rec {
-        pname = "haas-api";
-        version = "0.0.0";
-        src = ./.;
-        cargoLock.lockFile = ./Cargo.lock;
-        doCheck = false; # FIXME
-      };
+      packages.haas-api = pkgs.callPackage haasApiPackage { inherit system; };
+      packages.haas-api-dockerImage = if pkgs.lib.hasSuffix "linux" system then pkgs.dockerTools.buildLayeredImage {
+        name = "haas-api";
+        contents = pkgs.callPackage haasApiPackage { inherit system; /* FIXME */ systemd = true; };
+        extraCommands = ''
+          cp ${./Rocket.toml} .
+        '';
+        config = {
+          Cmd = [ "haas_api" ];
+        };
+      } else null;
 
       defaultPackage = packages.haas-api;
     });
